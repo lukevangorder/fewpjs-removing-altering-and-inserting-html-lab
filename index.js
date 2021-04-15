9b5bc7bfe2c470f90aaf0dd767f18850f07d8f87
lukevangorder <lukevangorder@gmail.com>
2021-04-14 21:14:52 -0400

    commit tree b51c089fc02cee507670b2dd1a354f3c6cabc90b

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code here!
+document.body.removeChild(document.body.querySelector('main'));
+let newHeader = document.createElement('h1');
+newHeader.setAttribute('id', 'victory');
+newHeader.innerHTML = "YOUR-NAME is the champion";
